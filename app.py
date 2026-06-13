ffa53d5da709bf5ca74a8b830e7e4da83aa1ba51
Priyabrata Mondal <priyabratamondal622@gmail.com>
2026-06-13 10:02:58 +0530

    Updated gemini key in .env environment

--- app.py
+++ app.py
@@ -63,6 +63,9 @@ reranker = CrossEncoder(
 # -------------------
 # Gemini
 # -------------------
+from dotenv import load_dotenv
+
+load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
